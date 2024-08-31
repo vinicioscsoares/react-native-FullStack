c5f6c4136aa8aba293d24b30f30fb66a773f0f54
Vinicios C Soares <vinicioscsoares@gmail.com>
2024-08-31 19:27:55 -0300

    home page, done

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,53 @@
-import { Text, View } from 'react-native'
+import { Text, View, Image, ScrollView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import React, { Component } from 'react'
 import { Link} from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+//import { ScrollView, GestureHandlerRootView } from 'react-native-gesture-handler'
+import { images } from '../constants'
+import  CustomButton from '../components/CustomButton'
 
-export default class RootLayout extends Component {
-  render() {
-    return (
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-3xl font-pblack ">Aura!</Text>
-        <StatusBar style='auto' />
-        <Link href="/home">Go Home</Link>
-      </View>
-    )
-  }
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height:'100%' }} >
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode='contain'
+          />
+          <Image 
+            source={images.cards}
+            className="max-w--[380px] w-full h-[298px]"
+            resizeMode='contain'
+          />
+          <View className="relative mt-5">
+            <Text className="text-3x1 text-white font-bold text-center">
+              Descover Endless {"\n"} Possibilities with{' '}
+            <Text className="text-secondary-200">Aura</Text>
+            </Text>
+
+            <Image 
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode='contain'
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+          <CustomButton 
+           title="Continue with Email"
+           handlePress={()=>{}}
+           containerStyles="w-full mt-7"
+          />
+
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light' />
+
+    </SafeAreaView>
+  )
 }
